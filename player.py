695532c6e53752d9f8d29e7bfbc0a05a7e067089
PurifiedMagic <ePurifiedMagic@gmail.com>
2024-09-06 19:51:27 +1000

    Asteroids v0.1.6: Implemented Player movement

--- player.py
+++ player.py
@@ -21,16 +21,27 @@ class Player(CircleShape):
     def draw(self, screen):
         self.player = pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
 
-        # Rotate left when "A" key is pressed
-        if keys[pygame.K_a]:
+        # Move forwards when "W" key is pressed
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(dt * -1)
+
+        # Move backwards when "S" key is pressed
+        if keys[pygame.K_a]  or keys[pygame.K_LEFT]:
             self.rotate(dt * -1)
 
         # Rotate right when "D" key is pressed
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
