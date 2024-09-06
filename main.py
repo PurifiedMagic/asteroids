f77785e6f878cd6961b042cac43cd36a9b12e804
PurifiedMagic <ePurifiedMagic@gmail.com>
2024-09-06 16:07:21 +1000

    Asteroids v0.1.2: Set game FPS

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         # Check for game window close event
         for event in pygame.event.get():
@@ -21,6 +24,9 @@ def main():
         # Set and update screen background fill colour to black
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
+        print(dt)
 
 
 
