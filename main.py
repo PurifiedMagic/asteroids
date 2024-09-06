b40614353f1c24dbf2d3a16af98c9f461fee0aee
PurifiedMagic <ePurifiedMagic@gmail.com>
2024-09-06 15:39:34 +1000

    Asteroids v0.1.1: Game window and loop setup

---
+++ main.py
@@ -0,0 +1,28 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+from constants import *
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        # Check for game window close event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # Set and update screen background fill colour to black
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
